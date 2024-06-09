5f95dcaeffcafd124ccf5c657f860f2a73387262
ahtisham225 <ahtisham225ali@gmail.com>
2024-06-09 17:47:06 +0500

    messages and chats column added to Drizzle ORM

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,11 +1,11 @@
-import type {Config} from 'drizzle-kit';
 import * as dotenv from 'dotenv';
-dotenv.config([ path: ".env"]);
-
-export default {
-    driver: "pg",
-    schema: './src/lib/db/schema.ts',
-    dbCredentials: {
-        connectionString: process.env.DATABASE_URL,
-    }
-} satisfies Config;
+dotenv.config();
+const DatabaseURL = process.env.DATABASE_URL;
+import { defineConfig, Config } from 'drizzle-kit'; // Add Config import
+export default defineConfig({
+  schema: './src/lib/db/schema.ts',
+  dialect: 'postgresql',
+  dbCredentials: {
+    url: DatabaseURL || '', // Assign a default value of empty string if DatabaseURL is undefined
+  }
+});
